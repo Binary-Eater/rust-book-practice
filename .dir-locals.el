9023d31fff24c309fee1c372e99cba1f18f4e19d
Rahul Rameshbabu <sergeantsagara@protonmail.com>
2023-08-19 19:17:35 -0700

    [chapter-1] Completed execises and INSTALL.org file
    Include a Emacs directory local variable file to make rust-analyzer work
    correctly with nested Rust projects inside top level projectile project.
    
    Signed-off-by: Rahul Rameshbabu <sergeantsagara@protonmail.com>

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((rust-mode . ((lsp-rust-analyzer-linked-projects . ["./chapter-1/hello_cargo/Cargo.toml"]))))
