3d3f881a28de086d217f2db50636b565323edf7c
Rahul Rameshbabu <sergeantsagara@protonmail.com>
2023-08-19 19:37:30 -0700

    [chapter-2] Initial guessing_game implementation
    Basic input/output program that takes input from stdin and prints it to stdout.
    
    Signed-off-by: Rahul Rameshbabu <sergeantsagara@protonmail.com>

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,5 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((rust-mode . ((lsp-rust-analyzer-linked-projects . ["./chapter-1/hello_cargo/Cargo.toml"]))))
+((rust-mode . ((lsp-rust-analyzer-linked-projects . ["./chapter-1/hello_cargo/Cargo.toml"
+                                                     "./chapter-2/guessing_game/Cargo.toml"]))))
